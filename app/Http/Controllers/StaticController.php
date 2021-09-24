36f43879f9bab329aa44bbbca692d6fce611cb60
Dragsun <lucas-du-97442@hotmail.fr>
2021-09-24 12:27:46 +0200

    first commit

---
+++ app/Http/Controllers/StaticController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Fruit;
+
+class StaticController extends Controller
+{
+    public function home()
+    {
+        dd(Fruit::all());
+        return view('welcome');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://kit.fontawesome.com/35c69bda3a.js" crossorigin="anonymous"></script>
+        <title>{{ env('APP_NAME') }}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <br>
+        <link rel="icon" href="{{asset('img/favicon.png')}}">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <!-- animate CSS -->
+        <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+        <!-- owl carousel CSS -->
+        <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+        <!-- themify CSS -->
+        <link rel="stylesheet" href="{{asset('css/themify-icons.css')}}">
+        <!-- flaticon CSS -->
+        <link rel="stylesheet" href="{{asset('css/flaticon.css')}}">
+        <!-- fontawesome CSS -->
+        <link rel="stylesheet" href="{{asset('fontawesome/css/all.min.css')}}">
+        <!-- magnific CSS -->
+        <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+        <link rel="stylesheet" href="{{asset('css/gijgo.min.css')}}">
+        <!-- niceselect CSS -->
+        <link rel="stylesheet" href="{{asset('css/nice-select.css')}}">
+        <!-- slick CSS -->
+        <link rel="stylesheet" href="{{asset('css/slick.css')}}">
+        <!-- style CSS -->
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    </head>
+    <body class="antialiased">
+        @dd(App\Models\Fruits::all())
+
+        @include('components.footer')
+    </body>
+</html>
